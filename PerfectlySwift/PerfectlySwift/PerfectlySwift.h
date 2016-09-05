5c067cbc411ad6da6157d7a4a9edd1abe30744a8
Siavash <siavashabbasalipour@iMac.local>
2016-09-05 15:51:42 +1000

    - initial commit

---
+++ PerfectlySwift/PerfectlySwift/PerfectlySwift.h
@@ -0,0 +1,19 @@
+//
+//  PerfectlySwift.h
+//  PerfectlySwift
+//
+//  Created by Siavash Abbasalipour on 5/09/2016.
+//  Copyright © 2016 MobileDen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PerfectlySwift.
+FOUNDATION_EXPORT double PerfectlySwiftVersionNumber;
+
+//! Project version string for PerfectlySwift.
+FOUNDATION_EXPORT const unsigned char PerfectlySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectlySwift/PublicHeader.h>
+
+
